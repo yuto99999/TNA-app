45eaf5f2b4a81307759b80b726b3d5fea9f92e07
yuto99999 <myuto2003@gmail.com>
2023-12-04 20:47:07 +0900

    Routerを追加し、リダイレクトできるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import './App.css';
-import Register from './components/register';
-import Login from './components/Login';
+import Register from './components/template/Authentication/register';
+import Login from './components/template/Authentication/Login';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './components/template/Header';
+import Logout from './components/template/Authentication/Logout';
 
 function App() {
   return (
-    <>
-      {/* <Register /> */}
-      <Login />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route
+          path="/Home"
+          element={
+            <>
+              <Header />
+              <Logout />
+            </>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
