01789de7f890aa50a5a0bfa7857767e07761ea28
yuto99999 <myuto2003@gmail.com>
2023-12-06 15:31:17 +0900

    ログアウト、プロフィールボタン追加

--- src/components/template/Header.tsx
+++ src/components/template/Header.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Box } from "@mui/material";
+import Logout from "../organisms/Authentication/Logout";
+import ProfileBtn from "../molecule/ProfileBtn";
+import TestBtn from "../atmos/TestBtn";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <Box display="flex">
+      <Logout />
+      <ProfileBtn />
+      <TestBtn />
+    </Box>
+  );
+};
 
-export default Header
+export default Header;
