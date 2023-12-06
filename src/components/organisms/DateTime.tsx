e7afa8633d0d18a9898f4b6c402cd7205044d148
yuto99999 <myuto2003@gmail.com>
2023-12-06 15:29:51 +0900

    日時表示させる

---
+++ src/components/organisms/DateTime.tsx
@@ -0,0 +1,35 @@
+import { Box, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+
+const weekday = ["日", "月", "火", "水", "木", "金", "土"];
+
+const DateTime = () => {
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+
+  useEffect(() => {
+    setInterval(() => {
+      let d = new Date();
+      let year = d.getFullYear();
+      let month = d.getMonth() + 1;
+      let day = d.getDate();
+      let dayofweek = d.getDay();
+      setDate(
+        year + "年" + month + "月" + day + "日" + "[" + weekday[dayofweek] + "]"
+      );
+      let hour = d.getHours().toString().padStart(2, "0");
+      let minute = d.getMinutes().toString().padStart(2, "0");
+      let seconds = d.getSeconds().toString().padStart(2, "0");
+      setTime(hour + ":" + minute + ":" + seconds);
+    }, 1000);
+  }, []);
+
+  return (
+    <Box textAlign="center" marginTop="7rem">
+      <Typography>{date}</Typography>
+      <Typography>{time}</Typography>
+    </Box>
+  );
+};
+
+export default DateTime;
