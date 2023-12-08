0c3afebc5468bf397fff0c28a06447afc8091651
yuto99999 <myuto2003@gmail.com>
2023-12-09 01:47:52 +0900

    プロフィール編集関連

---
+++ src/components/molecule/Btn/ProfileBtn.tsx
@@ -0,0 +1,24 @@
+import { Box, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+const ProfileBtn = () => {
+  const navigate = useNavigate();
+  const doEditProfile = () => {
+    navigate("/Profile");
+  };
+
+  return (
+    <Box>
+      <Button
+        variant="contained"
+        onClick={() => {
+          doEditProfile();
+        }}
+      >
+        プロフィール編集
+      </Button>
+    </Box>
+  );
+};
+
+export default ProfileBtn;
