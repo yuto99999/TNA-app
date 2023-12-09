8d3fd69f955ebdb6036fac2c7c48800904b4fc2f
yuto99999 <myuto2003@gmail.com>
2023-12-10 00:07:03 +0900

    デザイン完成

--- src/components/organisms/Authentication/register.tsx
+++ src/components/organisms/Authentication/register.tsx
@@ -25,46 +25,55 @@ export default function Register() {
   };
 
   return (
-    <Box width={"100%"} textAlign={"center"} marginTop="10rem">
-      <Typography>新規登録</Typography>
-      <Box
-        component="form"
-        sx={{
-          "& > :not(style)": { m: 1, width: "25ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      >
+    <Box width="100%" height="100vh" bgcolor="#F5F4EE">
+      <Box display="flex" flexDirection="column" alignItems="center" pt="11rem">
+        <Typography
+          fontSize="3rem"
+          fontFamily="游ゴシック"
+          fontWeight="bold"
+          color="#2864F0"
+          mb={2}
+        >
+          新規登録
+        </Typography>
         <TextField
-          id="outlined-basic"
+          required
+          id="email"
           label="メールアドレス"
-          variant="outlined"
+          autoComplete="email"
+          autoFocus
+          value={email}
           onChange={(e) => setEmail(e.target.value)}
+          sx={{ width: "25%", m: 1.5 }}
         />
-      </Box>
-      <Box
-        component="form"
-        sx={{
-          "& > :not(style)": { m: 1, width: "25ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      >
         <TextField
-          id="outlined-basic"
+          required
+          id="password"
           label="パスワード"
-          variant="outlined"
+          type="password"
+          value={password}
           onChange={(e) => setPassword(e.target.value)}
+          sx={{ width: "25%", m: 1 }}
         />
+        <Button
+          variant="contained"
+          onClick={() => {
+            doRegister();
+          }}
+          sx={{
+            width: "25%",
+            mt: 8,
+            mb: 2,
+            bgcolor: "#2864F0",
+            fontSize: "1.3rem",
+            fontFamily: "游ゴシック",
+            fontWeight: 600,
+            borderRadius: "5rem",
+          }}
+        >
+          登録
+        </Button>
       </Box>
-      <Button
-        variant="contained"
-        onClick={() => {
-          doRegister();
-        }}
-      >
-        登録
-      </Button>
     </Box>
   );
 }
