09427ec2e6abb2f75449184bc3909f507128eb3c
yuto99999 <myuto2003@gmail.com>
2023-12-09 22:15:55 +0900

    useRecord定義→使えない

---
+++ src/components/molecule/Hooks/useRecord.ts
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import { collection, onSnapshot, query, orderBy ,where } from "firebase/firestore";
+import { firebaseApp } from "../../..";
+
+const useRecord = (data: string, type: string) => {
+  const [documents, setDocuments] = useState([]);
+
+  useEffect(() => {
+    const firestore = firebaseApp.firestore;
+    const docRef = collection(firestore, data);
+    const queryRef = query(
+      docRef,
+      where("type", "==", type),
+      orderBy("createdAt")
+    );
+    const unsub = onSnapshot(queryRef, (snapshot) => {
+      let results: any = [];
+      snapshot.docs.forEach((doc) => {
+        results.push({ ...doc.data(), id: doc.id });
+      });
+      console.log(results);
+      setDocuments(results);
+    });
+    return () => unsub();
+  }, [data , type]);
+
+  return { documents };
+};
+
+export default useRecord;
