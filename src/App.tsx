1e56117b6829c4d85b212db29afc9d0471ad61f1
yuto99999 <myuto2003@gmail.com>
2023-12-10 16:25:11 +0900

    Router設定変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
 "use client";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
 import Register from "./components/organisms/Authentication/register";
 import Login from "./components/organisms/Authentication/Login";
 import Top from "./components/template/Top";
 import Profile from "./components/organisms/Profile";
 import Chat from "./components/organisms/Chat";
 import Record from "./components/organisms/Record";
+import { Box, ThemeProvider } from "@mui/material";
+import Sidebar from "./components/template/Sidebar";
+
+function Layout() {
+  return (
+    <>
+      <Sidebar />
+      <Outlet />
+    </>
+  );
+}
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route element={<Layout />}>
+          <Route path="/Home" element={<Top />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/Chat" element={<Chat />} />
+          <Route path="/Record" element={<Record />} />
+        </Route>
         <Route path="/" element={<Login />} />
         <Route path="/Register" element={<Register />} />
-        <Route path="/Home" element={<Top />} />
-        <Route path="/Profile" element={<Profile />} />
-        <Route path="/Chat" element={<Chat />} />
-        <Route path="/Record" element={<Record />} />
       </Routes>
     </Router>
   );
