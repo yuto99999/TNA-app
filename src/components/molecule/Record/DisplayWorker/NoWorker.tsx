b80fd6c3960a93dd9cb1f2f44ad1e6ff2a5ba91a
yuto99999 <myuto2003@gmail.com>
2024-01-14 18:59:55 +0900

    在宅、退勤だけの表示をするコンポーネント作成

--- src/components/molecule/Record/DisplayWorker/NoWorker.tsx
+++ src/components/molecule/Record/DisplayWorker/NoWorker.tsx
@@ -1,1 +1,82 @@
-export {}
+import React, { useState, useEffect } from "react";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  Timestamp,
+} from "firebase/firestore";
+import { firebaseApp } from "../../../..";
+import { Avatar, Box, Typography } from "@mui/material";
+import useRecord from "../../Hooks/useRecord";
+import Note from "../Note";
+
+interface Record {
+  createdAt: Timestamp;
+  type: string;
+  user: {
+    name: string;
+    uid: string;
+    image: string;
+  };
+}
+
+const NoWorker = () => {
+  const { documents: officeRecord } = useRecord("Records", "退勤");
+
+  const [records, setRecords] = useState<Array<any>>([]);
+  const db = firebaseApp.firestore;
+
+  useEffect(() => {
+    const fetchRecords = async () => {
+      const recordsRef = collection(firebaseApp.firestore, "Records");
+      const q = query(recordsRef, where("type", "==", "退勤"));
+
+      const querySnapshot = await getDocs(q);
+      const fetchedRecords = querySnapshot.docs.map((doc) => doc.data());
+      setRecords(fetchedRecords);
+    };
+
+    fetchRecords();
+  }, []);
+
+  return (
+    <Box display="flex">
+      <Box p={3} width="50%">
+        {officeRecord.map((Records: Record, index) => (
+          <Box
+            key={index}
+            width="auto"
+            display="flex"
+            alignItems="center"
+            bgcolor="#F5F4EE"
+            m={2}
+            mb={3}
+            p={2}
+            borderRadius={5}
+          >
+            <Avatar src={Records.user.image} sx={{ mr: 2 }} />
+            <Typography sx={{ ...styledFont, mr: 2 }}>
+              {Records.user.name}
+            </Typography>
+            <Typography sx={{ ...styledFont, mr: 2 }}>
+              {Records.type}
+            </Typography>
+            <Typography sx={{ ...styledFont, mr: 2 }}>
+              {Records.createdAt.toDate().toLocaleString()}
+            </Typography>
+          </Box>
+        ))}
+      </Box>
+      <Note />
+    </Box>
+  );
+};
+
+const styledFont = {
+  fontSize: "1.2rem",
+  fontFamily: "游ゴシック",
+  fontWeight: 600,
+};
+
+export default NoWorker;
--- src/components/molecule/Record/DisplayWorker/RemoteWorker.tsx
+++ src/components/molecule/Record/DisplayWorker/RemoteWorker.tsx
@@ -8,8 +8,8 @@ import {
 } from "firebase/firestore";
 import { firebaseApp } from "../../../..";
 import { Avatar, Box, Typography } from "@mui/material";
-import useFirebase from "../../Hooks/useFirebase";
 import useRecord from "../../Hooks/useRecord";
+import Note from "../Note";
 
 interface Record {
   createdAt: Timestamp;
@@ -21,8 +21,8 @@ interface Record {
   };
 }
 
-const OfficeWorker = () => {
-  const { documents: officeRecord } = useRecord("Records", "出社");
+const RemoteWorker = () => {
+  const { documents: officeRecord } = useRecord("Records", "在宅");
 
   const [records, setRecords] = useState<Array<any>>([]);
   const db = firebaseApp.firestore;
@@ -30,7 +30,7 @@ const OfficeWorker = () => {
   useEffect(() => {
     const fetchRecords = async () => {
       const recordsRef = collection(firebaseApp.firestore, "Records");
-      const q = query(recordsRef, where("type", "==", "出社"));
+      const q = query(recordsRef, where("type", "==", "在宅"));
 
       const querySnapshot = await getDocs(q);
       const fetchedRecords = querySnapshot.docs.map((doc) => doc.data());
@@ -41,29 +41,34 @@ const OfficeWorker = () => {
   }, []);
 
   return (
-    <Box p={3}>
-      {officeRecord.map((Records: Record, index) => (
-        <Box
-          key={index}
-          width="45%"
-          display="flex"
-          alignItems="center"
-          bgcolor="#F5F4EE"
-          m={2}
-          mb={3}
-          p={2}
-          borderRadius={5}
-        >
-          <Avatar src={Records.user.image} sx={{ mr: 2 }} />
-          <Typography sx={{ ...styledFont, mr: 2 }}>
-            {Records.user.name}
-          </Typography>
-          <Typography sx={{ ...styledFont, mr: 2 }}>{Records.type}</Typography>
-          <Typography sx={{ ...styledFont, mr: 2 }}>
-            {Records.createdAt.toDate().toLocaleString()}
-          </Typography>
-        </Box>
-      ))}
+    <Box display="flex">
+      <Box p={3} width="50%">
+        {officeRecord.map((Records: Record, index) => (
+          <Box
+            key={index}
+            width="auto"
+            display="flex"
+            alignItems="center"
+            bgcolor="#F5F4EE"
+            m={2}
+            mb={3}
+            p={2}
+            borderRadius={5}
+          >
+            <Avatar src={Records.user.image} sx={{ mr: 2 }} />
+            <Typography sx={{ ...styledFont, mr: 2 }}>
+              {Records.user.name}
+            </Typography>
+            <Typography sx={{ ...styledFont, mr: 2 }}>
+              {Records.type}
+            </Typography>
+            <Typography sx={{ ...styledFont, mr: 2 }}>
+              {Records.createdAt.toDate().toLocaleString()}
+            </Typography>
+          </Box>
+        ))}
+      </Box>
+      <Note />
     </Box>
   );
 };
@@ -74,4 +79,4 @@ const styledFont = {
   fontWeight: 600,
 };
 
-export default OfficeWorker;
+export default RemoteWorker;
